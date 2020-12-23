d92ed4aa553457705adcc146130c10e4cb098229
kaffe <35653840+kaffeenj@users.noreply.github.com>
2020-12-23 13:09:23 +0700

    add page loading progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,18 @@
-import Head from 'next/head';
+import Router from 'next/router';
+import NProgress from 'nprogress/nprogress';
+import 'nprogress/nprogress.css';
 import '../styles/globals.css';
+
+// Page loading progress bar.
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <div className="min-h-screen bg-gray-100 font-raleway text-gray-900">
-      <Head>
-        {/* Google Fonts (Poppins and Raleway)*/}
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
       <Component {...pageProps} />
     </div>
   );
