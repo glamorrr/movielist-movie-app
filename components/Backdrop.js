4f90daeeb3aae1c9bfbe81290d2f92a53e09d297
kaffe <35653840+kaffeenj@users.noreply.github.com>
2021-02-07 10:19:32 +0700

    change poster and backdrop size

--- components/Backdrop.js
+++ components/Backdrop.js
@@ -9,7 +9,7 @@ const Poster = ({ backdrop, imagesTMDbAPIConfiguration }) => {
       <article className="text-gray-500 transition-colors cursor-pointer hover:text-blue-400">
         <div className="bg-blue-100 shadow-md aspect-w-16 aspect-h-9">
           <Image
-            src={`${base_url}${backdrop_sizes[3]}${file_path}`}
+            src={`${base_url}${backdrop_sizes[1]}${file_path}`}
             alt="poster"
             layout="fill"
             objectFit="cover"
--- components/Poster.js
+++ components/Poster.js
@@ -9,7 +9,7 @@ const Poster = ({ poster, imagesTMDbAPIConfiguration }) => {
       <article className="text-gray-500 transition-colors cursor-pointer hover:text-blue-400">
         <div className="bg-blue-100 shadow-md aspect-w-2 aspect-h-3">
           <Image
-            src={`${base_url}${poster_sizes[6]}${file_path}`}
+            src={`${base_url}${poster_sizes[3]}${file_path}`}
             alt="poster"
             layout="fill"
             objectFit="cover"
