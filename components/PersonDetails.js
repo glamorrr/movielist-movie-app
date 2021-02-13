e27e86f0d881a5108c7d3bb86c8c0743b7b07999
kaffe <35653840+kaffeenj@users.noreply.github.com>
2021-02-13 17:30:12 +0700

    remove csstransition

--- components/PersonDetails.js
+++ components/PersonDetails.js
@@ -84,44 +84,39 @@ const PersonDetails = ({ person, imagesTMDbAPIConfiguration }) => {
                   </h2>
                   <div className="grid grid-cols-3 mx-auto mt-5 sm:grid-cols-4 md:grid-cols-5 gap-x-4 gap-y-5 md:gap-6 lg:gap-8">
                     {credits.map(({ character, id, job, poster_path, title }) => (
-                      <CSSTransition
-                        classNames="CSSTransitionScale"
+                      <Link
                         key={id + (character || job)}
-                        timeout={300}
-                        appear={true}
-                        in={true}
+                        href={`/movie/${id}-${parseToDashedString(title)}`}
                       >
-                        <Link href={`/movie/${id}-${parseToDashedString(title)}`}>
-                          <a className="block cursor-pointer font-poppins group">
-                            <div style={{ maxWidth: '185px' }}>
-                              <div className="overflow-hidden bg-blue-100 rounded-md shadow-lg aspect-w-2 aspect-h-3 lg:rounded lg:shadow-xl">
-                                <Image
-                                  src={
-                                    poster_path
-                                      ? `${base_url}${poster_sizes[2]}${poster_path}`
-                                      : "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%2393C5FD' fill-opacity='0.31'%3E%3Cpath d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E"
-                                  }
-                                  alt={title}
-                                  layout="fill"
-                                  objectFit="cover"
-                                />
-                              </div>
+                        <a className="block cursor-pointer font-poppins group">
+                          <div style={{ maxWidth: '185px' }}>
+                            <div className="overflow-hidden bg-blue-100 rounded-md shadow-lg aspect-w-2 aspect-h-3 lg:rounded lg:shadow-xl">
+                              <Image
+                                src={
+                                  poster_path
+                                    ? `${base_url}${poster_sizes[2]}${poster_path}`
+                                    : "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 40 40'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%2393C5FD' fill-opacity='0.31'%3E%3Cpath d='M0 38.59l2.83-2.83 1.41 1.41L1.41 40H0v-1.41zM0 1.4l2.83 2.83 1.41-1.41L1.41 0H0v1.41zM38.59 40l-2.83-2.83 1.41-1.41L40 38.59V40h-1.41zM40 1.41l-2.83 2.83-1.41-1.41L38.59 0H40v1.41zM20 18.6l2.83-2.83 1.41 1.41L21.41 20l2.83 2.83-1.41 1.41L20 21.41l-2.83 2.83-1.41-1.41L18.59 20l-2.83-2.83 1.41-1.41L20 18.59z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E"
+                                }
+                                alt={title}
+                                layout="fill"
+                                objectFit="cover"
+                              />
                             </div>
-                            <p className="mt-2 font-semibold text-gray-800 transition-colors group-hover:text-blue-500 line-clamp-1">
-                              {job || 'Actor'}
-                              {character && (
-                                <span className="text-sm text-gray-600 transition-colors group-hover:text-blue-400 font-raleway">
-                                  {' '}
-                                  {character}
-                                </span>
-                              )}
-                            </p>
-                            <p className="text-xs font-medium tracking-wide text-gray-500 transition-colors group-hover:text-blue-300">
-                              {title}
-                            </p>
-                          </a>
-                        </Link>
-                      </CSSTransition>
+                          </div>
+                          <p className="mt-2 font-semibold text-gray-800 transition-colors group-hover:text-blue-500 line-clamp-1">
+                            {job || 'Actor'}
+                            {character && (
+                              <span className="text-sm text-gray-600 transition-colors group-hover:text-blue-400 font-raleway">
+                                {' '}
+                                {character}
+                              </span>
+                            )}
+                          </p>
+                          <p className="text-xs font-medium tracking-wide text-gray-500 transition-colors group-hover:text-blue-300">
+                            {title}
+                          </p>
+                        </a>
+                      </Link>
                     ))}
                   </div>
                 </section>
--- components/Poster.js
+++ components/Poster.js
@@ -1,25 +1,22 @@
 import Image from 'next/image';
-import { CSSTransition } from 'react-transition-group';
 
 const Poster = ({ poster, imagesTMDbAPIConfiguration }) => {
   const { file_path } = poster;
   const { base_url, poster_sizes } = imagesTMDbAPIConfiguration;
 
   return (
-    <CSSTransition classNames="CSSTransitionScale" timeout={300} appear={true} in={true}>
-      <a href={`${base_url}${poster_sizes[6]}${file_path}`} target="_blank">
-        <article className="text-gray-500 transition-colors cursor-pointer hover:text-blue-400">
-          <div className="bg-blue-100 shadow-md aspect-w-2 aspect-h-3">
-            <Image
-              src={`${base_url}${poster_sizes[3]}${file_path}`}
-              alt="poster"
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-        </article>
-      </a>
-    </CSSTransition>
+    <a href={`${base_url}${poster_sizes[6]}${file_path}`} target="_blank">
+      <article className="text-gray-500 transition-colors cursor-pointer hover:text-blue-400">
+        <div className="bg-blue-100 shadow-md aspect-w-2 aspect-h-3">
+          <Image
+            src={`${base_url}${poster_sizes[3]}${file_path}`}
+            alt="poster"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+      </article>
+    </a>
   );
 };
 
